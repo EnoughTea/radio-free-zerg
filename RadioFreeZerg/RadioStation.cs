1b58436344f2e6f53145939d9418a8ae00f6598c
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 00:47:56 +0300

    Add a radio station model

---
+++ RadioFreeZerg/RadioStation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RadioFreeZerg
+{
+    public record RadioStation(int Id, string Title, string Genre, string Source)
+    {
+        public static RadioStation Default { get; } = new(0, "Default empty radio", "", "");
+
+        public Uri? ToUri() {
+            Uri.TryCreate(Source, UriKind.Absolute, out var parsedUri);
+            return parsedUri;
+        }
+
+        public async Task<bool> CanBeAccessed() {
+            var uri = ToUri();
+            if (uri != null)
+                try {
+                    var response = await SharedHttpClient.Instance.GetAsync(uri,
+                        HttpCompletionOption.ResponseHeadersRead);
+                    return response.IsSuccessStatusCode;
+                } catch {
+                    return false;
+                }
+
+            return false;
+        }
+    }
+}
