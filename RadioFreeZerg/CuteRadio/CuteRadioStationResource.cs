5943e923564d21bd862fe669eb63eee94acae664
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 00:57:59 +0300

    Add CuteRadio station models

---
+++ RadioFreeZerg/CuteRadio/CuteRadioStationResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RadioFreeZerg.CuteRadio
+{
+    /// <summary> A CuteRadio station resource identifies an internet radio station. </summary>
+    public record CuteRadioStationResource(int Id,
+                                           string Title,
+                                           string Description,
+                                           string Genre,
+                                           string Country,
+                                           string Language,
+                                           string Source,
+                                           int PlayCount,
+                                           DateTime LastPlayed,
+                                           int CreatorId,
+                                           bool Approved)
+    {
+        public RadioStation ToRadio() => new(Id, Title, Genre, Source);
+    }
+}
---
+++ RadioFreeZerg/CuteRadio/CuteRadioStationResources.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Nito;
+
+namespace RadioFreeZerg.CuteRadio
+{
+    /// <summary> CuteRadio response for GET /api/cuteradio/stations — retrieves a list of stations. </summary>
+    public class CuteRadioStationResources
+    {
+        public CuteRadioStationResource[]? Items { get; set; }
+
+        public string? Next { get; set; }
+
+        public string? Previous { get; set; }
+
+        public static Try<CuteRadioStationResources> Deserialize(string responseContent) =>
+            Try.Create(() => {
+                var result = JsonConvert.DeserializeObject<CuteRadioStationResources>(responseContent);
+                if (result == null)
+                    throw new InvalidDataException(
+                        "CuteRadio responded with empty JSON instead of station array.");
+                return result;
+            });
+
+        public IEnumerable<RadioStation> ToRadioStations() =>
+            Items != null && Items.Length > 0
+                ? Items.Select(_ => _.ToRadio())
+                : Enumerable.Empty<RadioStation>();
+    }
+}
