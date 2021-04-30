86d0acf15cae595d1b07011e814fe4a6bc9aaf91
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 00:47:12 +0300

    Add shared HttpClient

---
+++ RadioFreeZerg/SharedHttpClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace RadioFreeZerg
+{
+    /// <summary>
+    /// Provides shared <see cref="HttpClient"/> instance.
+    /// See https://www.aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/.
+    /// There could be certain DNS issues, so use <see cref="SetConnectionLeaseTimeout"/> if needed.
+    /// </summary>
+    public static class SharedHttpClient
+    {
+        public static HttpClient Instance { get; } = new HttpClient();
+
+        public static void SetConnectionLeaseTimeout(Uri uri, int timeout = -1) {
+            string uriBase = uri.GetLeftPart(UriPartial.Authority);
+            var servicePoint = ServicePointManager.FindServicePoint(new Uri(uriBase));
+            servicePoint.ConnectionLeaseTimeout = timeout;
+        }
+    }
+}
