4f940426a8525ef8db3b81ea0caf7cb80515414f
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 00:03:21 +0300

    Initial entry point

---
+++ RadioFreeZerg/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RadioFreeZerg
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args) => Console.ReadKey();
+    }
+}
