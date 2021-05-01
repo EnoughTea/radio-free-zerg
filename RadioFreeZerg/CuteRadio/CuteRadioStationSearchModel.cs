17df7b91d0bde51491f07d3ebc8a909de97dcfb4
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 19:01:45 +0300

    Add proper ctor to a search model

--- RadioFreeZerg/CuteRadio/CuteRadioStationSearchModel.cs
+++ RadioFreeZerg/CuteRadio/CuteRadioStationSearchModel.cs
@@ -5,41 +5,51 @@ namespace RadioFreeZerg.CuteRadio
     /// <summary>
     ///     Represents CuteRadio search capabilites.
     /// </summary>
-    public record CuteRadioStationSearchModel
+    public record CuteRadioStationSearchModel(int Offset = 0,
+                                              int Limit = 10,
+                                              string Search = "",
+                                              string Country = "",
+                                              string Genre = "",
+                                              string Language = "",
+                                              string Id = "",
+                                              CuteRadioStationSort Sort = CuteRadioStationSort.Title,
+                                              bool SortDescending = false)
     {
         /// <summary> Restricts results to those matching the specified country. </summary>
-        public string Country { get; init; } = "";
+        public string Country { get; } = Country;
 
         /// <summary> Restricts results to those matching the specified genre. </summary>
-        public string Genre { get; init; } = "";
+        public string Genre { get; } = Genre;
 
         /// <summary> Restricts results to those matching the comma-separated list of station ids. </summary>
-        public string Id { get; init; } = "";
+        public string Id { get; } = Id;
 
         /// <summary> Restricts results to those matching the specified language. </summary>
-        public string Language { get; init; } = "";
+        public string Language { get; } = Language;
 
         /// <summary>
         ///     The maximum number of results that should be returned. Must be between 1 and 50. The default is 20.
         /// </summary>
-        public int Limit { get; init; } = 10;
+        public int Limit { get; } = Limit;
 
         /// <summary> The index of the first result to be returned. The index is 0-based. </summary>
-        public int Offset { get; init; }
+        public int Offset { get; } = Offset;
 
         /// <summary> The keyword(s) to be used to filter results. </summary>
-        public string Search { get; init; } = "";
+        public string Search { get; } = Search;
 
         /// <summary> The property used to sort the results. The default is 'title'. </summary>
-        public CuteRadioStationSort Sort { get; init; } = CuteRadioStationSort.Title;
+        public CuteRadioStationSort Sort { get; } = Sort;
 
         /// <summary> Whether result should be sorted in descending order. The default is false. </summary>
-        public bool SortDescending { get; init; }
+        public bool SortDescending { get; } = SortDescending;
 
         /// <summary>
         ///     Restricts results to those created by the authenticated user. Requires a valid access token.
         /// </summary>
-        public bool Mine { get; init; }
+        //public bool Mine { get; init; }
+        public static CuteRadioStationSearchModel FromSearch(string search, int offset = 0, int limit = 10) =>
+            new(offset, limit, search);
 
         public RestRequest ToRequest() =>
             (RestRequest) new RestRequest("stations", Method.GET)
