7f414a22ea0ddc77c04779f2d64b58b386c231d9
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 19:00:20 +0300

    Add REST client instance for CuteRadio

---
+++ RadioFreeZerg/CuteRadio/CuteRadioClient.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace RadioFreeZerg.CuteRadio
+{
+    public class CuteRadioClient
+    {
+        private const string ApiEndpoint = "http://marxoft.co.uk/api/cuteradio/";
+        
+        /// <summary> RestClient is thread-safe except for handler operations. </summary>
+        public static RestClient Instance { get; } = new(ApiEndpoint);
+    }
+}
