aca22dd810757c6f20aac03c0bd30a44444606f0
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-01 21:05:05 +0300

    Test stuff in Program. Who needs unit tests?

--- RadioFreeZerg/Program.cs
+++ RadioFreeZerg/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Threading.Tasks;
+using NLog;
+using RadioFreeZerg.CuteRadio;
 
 namespace RadioFreeZerg
 {
     internal class Program
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        
         private static async Task Main(string[] args) {
-            var unavailableRadio = new RadioStation(0, "NA", "NA", "http://balkan.dj.topstream.net:8070");
-            var (uri, headers) = await unavailableRadio.FindStreamUriAsync().ConfigureAwait(false);
+            var stationsPage = await CuteRadioStationsPage.FetchAsync(CuteRadioStationSearchModel.FromSearch("", 0, 10))
+                                                          .ConfigureAwait(false);
+            var nextPage = await stationsPage.FetchNextAsync().ConfigureAwait(false);
+            var radioStations = stationsPage.Stations.ToRadioStations();
+            
+            var testStation = RadioStation.FromRawSource(0, "NA", "NA", "http://balkan.dj.topstream.net:8070");
+            var (uri, headers) = await testStation.FindStreamUriAsync().ConfigureAwait(false);
             Console.WriteLine($"{uri.AbsoluteUri} [{string.Join(", ", headers)}]");
         }
     }
