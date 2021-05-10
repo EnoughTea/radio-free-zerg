116424b4e4c5e0ce52f71992592ac7296ab5c69d
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-11 01:01:12 +0300

    Lock CurrentPage just in case

--- RadioFreeZerg/States/AppStateSearchData.cs
+++ RadioFreeZerg/States/AppStateSearchData.cs
@@ -8,6 +8,7 @@ namespace RadioFreeZerg.States
     public class AppStateSearchData
     {
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private readonly object locker = new();
 
         public CuteRadioStationResources? CurrentPage { get; set; }
 
@@ -22,26 +23,36 @@ namespace RadioFreeZerg.States
         public bool HasPrevious => !string.IsNullOrWhiteSpace(CurrentPage?.Previous);
 
         public void GoNext() {
-            if (HasNext) {
-                CurrentPage = NextPage;
-                SearchModel = SearchModel with {Offset = SearchModel.Offset + SearchModel.Limit};
+            lock (locker) {
+                if (HasNext) {
+                    CurrentPage = NextPage;
+                    NextPage = null;
+                    SearchModel = SearchModel with {Offset = SearchModel.Offset + SearchModel.Limit};
+                }
             }
         }
 
         public void GoPrevious() {
-            if (HasPrevious) {
-                CurrentPage = PreviousPage;
-                SearchModel = SearchModel with {Offset = SearchModel.Offset - SearchModel.Limit};
+            lock (locker) {
+                if (HasPrevious) {
+                    CurrentPage = PreviousPage;
+                    PreviousPage = null;
+                    SearchModel = SearchModel with {Offset = SearchModel.Offset - SearchModel.Limit};
+                }
             }
         }
 
         public Task Prefetch() {
-            if (CurrentPage is null) return Task.CompletedTask;
+            lock (locker) {
+                if (CurrentPage is null) return Task.CompletedTask;
+            }
 
             var fetchPrevTask = Task.Run(() => {
                 try {
-                    PreviousPage ??= CurrentPage.FetchPreviousOrNullAsync().ConfigureAwait(false)
-                                                .GetAwaiter().GetResult();
+                    lock (locker) {
+                        PreviousPage ??= CurrentPage.FetchPreviousOrNullAsync().ConfigureAwait(false)
+                                                    .GetAwaiter().GetResult();
+                    }
                 } catch (Exception e) {
                     Log.Error(e, $"Exception when prefetching previous search page {CurrentPage?.Previous}");
                 }
@@ -49,8 +60,10 @@ namespace RadioFreeZerg.States
 
             var fetchNextTask = Task.Run(() => {
                 try {
-                    NextPage ??= CurrentPage.FetchNextOrNullAsync().ConfigureAwait(false)
-                                            .GetAwaiter().GetResult();
+                    lock (locker) {
+                        NextPage ??= CurrentPage.FetchNextOrNullAsync().ConfigureAwait(false)
+                                                .GetAwaiter().GetResult();
+                    }
                 } catch (Exception e) {
                     Log.Error(e, $"Exception when prefetching next search page {CurrentPage?.Next}");
                 }
