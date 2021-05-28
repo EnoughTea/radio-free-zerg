26aa6e83ba39352ab506397489f61190178dedfb
Konstantin Zakharov <enough.tea@outlook.com>
2021-05-29 01:19:48 +0300

    Station manager for audio experiments

---
+++ RadioFreeZerg/RadioStationManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RadioFreeZerg
+{
+    public class RadioStationManager : IEnumerable<RadioStation>
+    {
+        private readonly AudioPlayer player;
+        private readonly Dictionary<int, RadioStation> stations;
+
+        public RadioStationManager(ICuteRadioStationProvider radioStationsProvider,
+                                   AudioPlayer audioPlayer) {
+            player = audioPlayer;
+            stations = radioStationsProvider.Select(_ => _.ToRadioStation())
+                                            .ToDictionary(_ => _.Id);
+        }
+
+        public IReadOnlyCollection<RadioStation> AllStations => stations.Values;
+        
+        public RadioStation CurrentStation { get; private set; } = RadioStation.Empty;
+
+        public IEnumerator<RadioStation> GetEnumerator() =>
+            ((IEnumerable<RadioStation>) stations.Values).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Play(int stationId) {
+            if (!stations.TryGetValue(stationId, out var station))
+                throw new ArgumentException("Invalid station ID", nameof(stationId));
+
+            Play(station);
+        }
+
+        public void Play(RadioStation station) {
+            if (station == RadioStation.Empty)
+                Stop();
+            else
+                lock (player) {
+                    if (station != CurrentStation) {
+                        CurrentStation = station;
+                        player.Play(station.Source);
+                    }
+                }
+        }
+
+        public void Stop() {
+            lock (player) {
+                player.Stop();
+                CurrentStation = RadioStation.Empty;
+            }
+        }
+    }
+}
