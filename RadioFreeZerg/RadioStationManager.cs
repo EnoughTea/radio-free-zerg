d806999c767ee725fafe1132e001ca7aa56f36b7
Konstantin Zakharov <enough.tea@outlook.com>
2021-06-01 17:17:12 +0300

    Remember previous station for toggling

--- RadioFreeZerg/RadioStationManager.cs
+++ RadioFreeZerg/RadioStationManager.cs
@@ -7,32 +7,34 @@ namespace RadioFreeZerg
 {
     public class RadioStationManager : IEnumerable<RadioStation>
     {
-        private readonly Dictionary<int, RadioStation> stations;
         private readonly RadioStationFinder finder;
         private readonly RadioStationPlayer player;
+        private readonly Dictionary<int, RadioStation> stations;
+        private RadioStation toggledStation;
 
         public RadioStationManager(ICuteRadioStationProvider radioStationsProvider) {
             stations = radioStationsProvider.Select(_ => _.ToRadioStation())
                                             .ToDictionary(_ => _.Id);
+            toggledStation = RadioStation.Empty;
             finder = new RadioStationFinder();
             player = new RadioStationPlayer(this);
         }
 
         public string NowPlaying => player.NowPlaying;
 
-        public event Action<string> NowPlayingChanged {
-            add => player.NowPlayingChanged += value;
-            remove => player.NowPlayingChanged -= value;
-        }
-        
         public RadioStation CurrentStation => player.CurrentStation;
-        
+
         public IReadOnlyCollection<RadioStation> All => stations.Values;
 
         public IEnumerator<RadioStation> GetEnumerator() =>
             ((IEnumerable<RadioStation>) stations.Values).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public event Action<string> NowPlayingChanged {
+            add => player.NowPlayingChanged += value;
+            remove => player.NowPlayingChanged -= value;
+        }
 
         public RadioStation Find(int stationId) {
             if (!stations.TryGetValue(stationId, out var foundStation)) foundStation = RadioStation.Empty;
@@ -45,14 +47,19 @@ namespace RadioFreeZerg
         public void Play(RadioStation stationToPlay) {
             player.Play(stationToPlay);
         }
-        
+
         public void Stop() {
             player.Stop();
         }
 
         public void TogglePlay(RadioStation stationToPlay) {
-            if (CurrentStation == stationToPlay) Stop();
-            else Play(stationToPlay);
+            if (CurrentStation != RadioStation.Empty) {
+                toggledStation = CurrentStation;
+                if (stationToPlay != CurrentStation) Play(stationToPlay);
+                else Stop();
+            } else {
+                Play(stationToPlay != RadioStation.Empty ? stationToPlay : toggledStation);
+            }
         }
     }
 }
