93db32608d633555bca4f17cc35c483f156d9e61
Konstantin Zakharov <enough.tea@outlook.com>
2021-06-02 19:07:58 +0300

    Improve search keywords handling

--- RadioFreeZerg/RadioStationFinder.cs
+++ RadioFreeZerg/RadioStationFinder.cs
@@ -7,18 +7,23 @@ namespace RadioFreeZerg
 {
     public class RadioStationFinder
     {
+        private static readonly HashSet<string> StopWords = new() {
+            "a", "an", "the", "and", "but", "or", "as", "of", "at", "by", "for", "with", "to", "s", "t"
+        };
+
         public IEnumerable<RadioStation> Find(string userInput, IReadOnlyCollection<RadioStation> radioStations) {
             var keywords = RemoveNoise(Regex.Split(userInput, @"\s+", RegexOptions.IgnoreCase,
-                TimeSpan.FromSeconds(1)).ToList());
+                TimeSpan.FromSeconds(1)));
             return from station in radioStations
                    from keyword in keywords
                    where station.Title.Contains(keyword) || station.Description.Contains(keyword)
                    select station;
         }
-        
-        private static List<string> RemoveNoise(List<string> rawKeywords) {
-            rawKeywords.Remove("a");
-            return rawKeywords;
+
+        private static IEnumerable<string> RemoveNoise(string[] rawKeywords) {
+            var deduplicatedKeywords = rawKeywords.ToHashSet(StringComparer.OrdinalIgnoreCase);
+            deduplicatedKeywords.ExceptWith(StopWords);
+            return deduplicatedKeywords;
         }
     }
 }
