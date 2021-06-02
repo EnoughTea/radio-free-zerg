c9b5b678b43e9fabd5243aac657563aa3e9b4b0d
Konstantin Zakharov <enough.tea@outlook.com>
2021-06-02 16:55:01 +0300

    Make InputWindow translation-ready

--- RadioFreeZerg/Gui/InputWindow.cs
+++ RadioFreeZerg/Gui/InputWindow.cs
@@ -5,6 +5,9 @@ namespace RadioFreeZerg.Gui
 {
     public class InputPrompt : Dialog
     {
+        public const string DefaultConfirmationText = "Ok";
+        public const string DefaultCancelText = "Cancel";
+        
         private readonly Button cancelButton;
         private readonly Label inputLabel;
         private readonly TextField inputTextField;
@@ -18,19 +21,25 @@ namespace RadioFreeZerg.Gui
         ///     Initializes a new instance of <see cref="T:Terminal.Gui.FileDialog" />
         /// </summary>
         /// <param name="title">The title.</param>
-        /// <param name="prompt">The prompt.</param>
+        /// <param name="confirmationButtonText">OK button text.</param>
+        /// <param name="cancelButtonText">Cancel button text.</param>
         /// <param name="message">The message.</param>
-        public InputPrompt(ustring title, ustring prompt, ustring message)
-            : this(title, prompt, ustring.Empty, message) { }
+        public InputPrompt(ustring title, ustring confirmationButtonText, ustring cancelButtonText, ustring message)
+            : this(title, confirmationButtonText, cancelButtonText, ustring.Empty, message) { }
 
         /// <summary>
         ///     Initializes a new instance of <see cref="T:Terminal.Gui.FileDialog" />
         /// </summary>
         /// <param name="title">The title.</param>
-        /// <param name="promptButtonText">The prompt.</param>
+        /// <param name="confirmationButtonText">OK button text.</param>
+        /// <param name="cancelButtonText">Cancel button text.</param>
         /// <param name="inputLabelText">The text of the input field label.</param>
         /// <param name="message">The message.</param>
-        public InputPrompt(ustring title, ustring promptButtonText, ustring inputLabelText, ustring message)
+        public InputPrompt(ustring title,
+                           ustring confirmationButtonText,
+                           ustring cancelButtonText,
+                           ustring inputLabelText,
+                           ustring message)
             : base(title) {
             messageLabel = new Label(message) {X = 1, Y = 0};
             Add(messageLabel);
@@ -48,19 +57,18 @@ namespace RadioFreeZerg.Gui
                 Width = Dim.Fill() - 1
             };
             inputTextField.TextChanged += _ => {
-                if (okButton != null)
-                    okButton.CanFocus = !inputTextField.Text.IsEmpty;
+                if (okButton != null) okButton.CanFocus = !inputTextField.Text.IsEmpty;
             };
             Add(inputLabel, inputTextField);
 
-            cancelButton = new Button("Cancel");
+            cancelButton = new Button(cancelButtonText.IsEmpty ? DefaultCancelText : cancelButtonText);
             cancelButton.Clicked += () => {
                 Canceled = true;
                 Application.RequestStop();
             };
             AddButton(cancelButton);
 
-            okButton = new Button(promptButtonText.IsEmpty ? "Ok" : promptButtonText) {
+            okButton = new Button(confirmationButtonText.IsEmpty ? DefaultConfirmationText : confirmationButtonText) {
                 IsDefault = true,
                 CanFocus = !inputTextField.Text.IsEmpty
             };
@@ -105,10 +113,11 @@ namespace RadioFreeZerg.Gui
         public bool Canceled { get; private set; }
 
         public static (string, bool) Display(string title,
-                                             string promptButtonText = "",
+                                             string confirmationButtonText = DefaultConfirmationText,
+                                             string cancelButtonText = DefaultCancelText,
                                              string inputLabelText = "",
                                              string message = "") {
-            var prompt = new InputPrompt(title, promptButtonText, inputLabelText, message);
+            var prompt = new InputPrompt(title, confirmationButtonText, inputLabelText, message);
             Application.Run(prompt);
             return (prompt.Input.IsEmpty ? "" : prompt.Input.ToString() ?? "", prompt.Canceled);
         }
